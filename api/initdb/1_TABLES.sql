2fc24fbf5f42bedcdade585d3dd22b0c5590e95c
TheRealSumpark <sumpark@hotmail.fr>
2022-10-04 15:30:19 +0100

    initial commit

--- api/initdb/1_TABLES.sql
+++ api/initdb/1_TABLES.sql
@@ -3,9 +3,13 @@ create table users
     uid VARCHAR(255),
     constraint users_pk
         primary key (uid),
-    display_name TEXT not null,
-    email TEXT not null,
-    profile_url TEXT not null
+    display_name varchar(255) not null,
+    email varchar(255) not null,
+    profile_url varchar(255) not null,
+    birthdate DATETIME not null,
+    nom varchar(255) not null,
+    prenom varchar(255) not null
+
 );
 
 create table books
@@ -13,9 +17,9 @@ create table books
     id bigint auto_increment,
     constraint books_pk
         primary key (id),
-    borrower_id TEXT,
-    author TEXT not null,
-    title TEXT not null
+    borrower_id varchar(255),
+    author varchar(255) not null,
+    title varchar(255) not null
 );
 
 create table rooms
@@ -23,19 +27,19 @@ create table rooms
     id bigint auto_increment,
     constraint rooms_pk
         primary key (id),
-    nom TEXT not null,
-    capacite TEXT not null,
-    equipements TEXT not null,
-    photo TEXT not null
+    nom varchar(255) not null,
+    capacite integer not null,
+    equipements varchar(255) not null,
+    photo varchar(255) not null
 );
 
-create table reservation
+create table reservations
 (
     id bigint auto_increment,
     constraint rooms_pk
         primary key (id),
-    hoteId bigint not null,
-    listInvites TEXT not null,
-    dateDebut DATETIME not null,
-    Datefin DATETIME not null
+    list_invites varchar(255) not null,
+    date_debut DATETIME not null,
+    date_fin DATETIME not null,
+    hote_id bigint not null
 );
--- api/src/main/java/io/takima/demo/Entities/User.kt
+++ api/src/main/java/io/takima/demo/Entities/User.kt
@@ -11,7 +11,7 @@ data class User(
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Id var uid: String?,
         @Column(name = "nom") var nom: String?,
-        @Column(name = "prénom") var prénom: String?,
+        @Column(name = "prenom") var prenom: String?,
         @Column(name = "email") var email: String?,
         @Column(name = "birthdate") var birthdate: Date?) {
     constructor() : this(null, null, null, null, null)
