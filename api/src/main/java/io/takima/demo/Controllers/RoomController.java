c31aac8893f764e14ef7b876fece52c0ea33292b
TheRealSumpark <sumpark@hotmail.fr>
2022-10-20 17:00:48 +0100

    added photo upload

--- api/src/main/java/io/takima/demo/Controllers/RoomController.java
+++ api/src/main/java/io/takima/demo/Controllers/RoomController.java
@@ -2,11 +2,14 @@ package io.takima.demo.Controllers;
 
 import io.takima.demo.DAO.RoomDAO;
 import io.takima.demo.Entities.Room;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.util.*;
 
 @RestController
 @RequestMapping("/rooms")
@@ -37,15 +40,48 @@ public class RoomController {
     @PostMapping("/add")
     public Room addRoom(@RequestBody Room room) {
 
-
         return this.roomDAO.save(room);
+    }
+
+
+    @PostMapping(path= "/upload")
+
+    public Optional<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+        String message;
+        Path root = Paths.get("./front/src/assets");
+//        try {
+//            Files.createDirectory(root);
+//        } catch (IOException e) {
+//            throw new RuntimeException("Could not initialize folder for upload!");
+//        }
+
+        String filename = file.getOriginalFilename();
+
+
+        long now = new Timestamp(System.currentTimeMillis()).getTime();
+        String extension = filename.substring(filename.lastIndexOf("."));
+        String new_filename = Long.toString(now).concat(extension);
+        try {
+            try {
+                Files.copy(file.getInputStream(), root.resolve(new_filename));
+
+            } catch (Exception e) {
+                throw new RuntimeException("FAIL!");
+            }
+            message = "Successfully uploaded!";
+            return Optional.of(new_filename);
+        }catch (Exception e)
+        {
+            return Optional.empty();
+        }
+
+
     }
 
 
     @PostMapping("/update")
     public Room updateRoom(@RequestBody Room room)
     {
-
         return this.roomDAO.save(room);
     }
 
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:8080'
+  url: 'http://localhost:8080',
+  uploadsFolder : 'uploads'
 };
 
